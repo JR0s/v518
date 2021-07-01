apply correct χ² calculation for all fits

pulshohe.py
@@ -13,6 +13,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+from utils import χ_sq
+
 def func(x, a, mu, sigma): #naehere Landau-Verteilung durch folgenden Ausdruck an
     lambd = (x-mu)/sigma
     return a/(np.sqrt(2*np.pi))*np.exp(-0.5*(lambd+np.exp(-lambd)))
@@ -40,8 +42,8 @@ for i in range(len(lines)):
     head, sep, tail = text.partition(',')
     x = np.append(x,float(head))
     y = np.append(y,float(tail))
-    
-    
+
+
 y_err = np.sqrt(y) #erzeuge y-Fehler durch Wurzel nehmen
 
 #Plotte die Funktion der gesamnten Daten (einbegriffen des Rauschens)
@@ -81,24 +83,21 @@ print(np.sqrt(pcov1[2][2]))
 print("----------")
 
 #Berechnung des Chi-Quadrats Landau: nach X =  sum(gemessen-erwartet)^2/erwartet)/N_ges
+# fix y errors. If 0 set to 1
+y_err[y_err == 0.0] = 1.0
+χ_sq_landau = χ_sq(func, x, popt, y, y_err)
+print("Landau:\n\tχ² = {}".format(χ_sq_landau))
+dof = len(x) - len(popt)
+print("\tχ²/dof = {}".format(χ_sq_landau / dof))
 
-chi2 = 0
-yer = 0
-for i in range(len(x)):
-    yer = func(x[i], popt[0], popt[1],popt[2])
-    chi2 =  chi2 + (y[i] - yer)**2/yer/14322
-
-print("Güte von Landau = ", chi2)
 print("---")
 
-for i in range(len(x)):
-    yer = func2(x[i], popt1[0], popt1[1],popt1[2])
-    chi2 =  chi2 + (y[i] - yer)**2/yer/14322
+χ_sq_gauss = χ_sq(func2, x, popt, y, y_err)
+print("Gauß:\n\tχ² = {}".format(χ_sq_gauss))
+dof = len(x) - len(popt)
+print("\tχ²/dof = {}".format(χ_sq_gauss / dof))
 
-    
-print("Güte von Gauß = ", chi2)
 
-    
 #Plotte die Funktion des eingeschraenkten Intervalls sowie den Fit mit y-Fehlern
 plt.figure()
 plt.grid(color='grey', linestyle='-', linewidth=0.1)

winkelverteilung.py
@@ -10,6 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+from utils import χ_sq
+
 def func(x, a, n): #definiere Winkelverteilung
     return a*np.power(np.cos(np.radians(x)), n)
 
@@ -26,7 +28,7 @@ def func(x, a, n): #definiere Winkelverteilung
 #10-22: -30 Grad
 #11-23: -15 Grad
 #12-24: 0 Grad
-    
+
 
 #Bestimme Zaehlraten und entsprechende Winkelwerte
 x = np.array([-75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75, 90])
@@ -66,28 +68,21 @@ print("----------")
 x_plot = np.array([]) #erstelle Array zur genaueren Darstellung der Anpassungsfunktion
 for i in range(1651):
     x_plot = np.append(x_plot, i*0.1-75)
-    
+
 
 #Berechnung des Chi-Quadrats mit Datenpunkt  bei  0: nach X =  sum(gemessen-erwartet)^2/erwartet)/N_ges
+χ_sq_val_full = χ_sq(func, x, popt, y, y_err)
+print("Full:\n\tχ² = {}".format(χ_sq_val_full))
+dof = len(x) - len(popt)
+print("\tχ²/dof = {}".format(χ_sq_val_full / dof))
 
-chi2 = 0
-yer = 0
+print("------------------------------")
 
-for i in range(len(x)-1):
-    yer = func(x[i], popt[0], popt[1])
-    chi2 =  chi2 + (y[i] - yer)**2/(yer*26196)
+χ_sq_val_wo_0 = χ_sq(func, xFit, poptOhneN, yFit, y_errFit)
+print("\ 0 :\n\tχ² = {}".format(χ_sq_val_wo_0))
+dof = len(x) - len(popt)
+print("\tχ²/dof = {}".format(χ_sq_val_wo_0 / dof))
 
-
-
-print("Güte mit berücksichtigung von 0 = ", chi2)
-
-chi2 = 0
-for i in range(len(xFit)):
-    yer = func(x[i], poptOhneN[0], poptOhneN[1])
-    chi2 =  chi2 + (yFit[i] - yer)**2/(yer*(26196-3038))
-
-print("Güte ohne berücksichtigung von 0 = ", chi2)
-    
 
 #Plotte die Funktion und Anpassung
 plt.figure()